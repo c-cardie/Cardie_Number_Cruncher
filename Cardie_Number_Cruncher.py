60a05e4d92f03a42c4b7447d8c10ee11b89751f7
c-cardie <ccardie001@student.franciscan.edu>
2025-04-30 19:03:51 -0400

    added documentation

--- Cardie_Number_Cruncher.py
+++ Cardie_Number_Cruncher.py
@@ -3,40 +3,48 @@ import concurrent.futures
 import time
 import os
 
+'''
+summing an array of 100,000 numbers in parallel
+math breakdown:
+    10 processes
+    array split - 10,000 arrays of 10 numbers
+    chunksize = 100 -> 10 processes get 100 arrays at a time to process
+'''
 
-#an array of numbers 0-99
+#an array of numbers 0-99999
 orig_array = np.arange(100000)
-#print("original array",'\n', orig_array)
-
 
 #split array into an array of arrays of 10
+#array_chunks holds 10,000 arrays of 10 numbers
 array_chunks = ([orig_array[i*10:(i*10)+10] for i in range(0,10000,1)])
 
-#print("split arrays",'\n', array_chunks)
-
-
+#summing_chunks takes an array_chunk of 10 numbers
+#prints process ID of current process and the array_chunk that has been passed in
+#returns the sum
 def summing_chunks(array_chunk):
     print(f"Process {os.getpid()} is computing {array_chunk}...", '\n')
     return sum(array_chunk)
 
-
 def main():
     
+    #create the pool of "Process" objects (not to be confused w/ threads!)
+    #that are named "executor"
     with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
 
-        chunk_sums = []
+        start = time.perf_counter() #start timer
 
-        start = time.perf_counter()
+        #chunk_sums list holds a list of each of the sums of the 10,000 arrays
+        #executor.map... 
+            #makes each of the 10 processes run the summing_chunks(array_chunks[whatever])
         chunk_sums = list(executor.map(summing_chunks, array_chunks, chunksize=1000))
-        finish = time.perf_counter()
+        finish = time.perf_counter() #stop timer
 
-    #print("chunk_sums: ",'\n', chunk_sums)
-
+    #take the sum of the 10,000 sums to get the total sum
     total_sum = sum(chunk_sums)
     print("total sum: ",'\n', total_sum)
 
-    print(f'Finished in {round(finish-start, 2)} seconds(s)')
-    
+    #print time it took program to run in parallel
+    print(f'Finished in {round(finish-start, 2)} seconds(s)')   
 
 if __name__ == '__main__':
     main()
--- Cardie_Number_Cruncher_Sequential.py
+++ Cardie_Number_Cruncher_Sequential.py
@@ -3,31 +3,32 @@ import concurrent.futures
 import time
 import os
 
+'''
+summing an array of 100,000 numbers sequentially
+math breakdown:
+    1 process
+    array split - 10,000 arrays of 10 numbers
+    chunksize = 100 -> 1 process gets 100 arrays at a time to process
+'''
+#see the parallel version for more extensive version of comments
+
 #an array of numbers 0-99
 orig_array = np.arange(100000)
-#print("original array",'\n', orig_array)
 
 #split array into an array of arrays of 10
 array_chunks = ([orig_array[i*10:(i*10)+10] for i in range(0,10000,1)])
-
-#print("split arrays",'\n', array_chunks)
 
 def summing_chunks(array_chunk):
     print(f"Process {os.getpid()} is computing {array_chunk}...", '\n')
     return sum(array_chunk)
 
-
 def main():
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
 
-        chunk_sums = []
-
         start = time.perf_counter()
         chunk_sums = list(executor.map(summing_chunks, array_chunks, chunksize=1000))
         finish = time.perf_counter()
-    
-    #print("chunk_sums: ",'\n', chunk_sums)
 
     total_sum = sum(chunk_sums)
     print("total sum: ",'\n', total_sum)
